5bc380a40477cb7e29e60417d5febf9ce0af17bc
Desutoka <47184769+Desutoka@users.noreply.github.com>
2021-06-16 23:51:14 +0300

    Delete valid.php

--- test1/Потом/valid.php
+++
@@ -1,8 +0,0 @@
-<?php $log=$_POST['name'];$pas=$_POST['pas'];
-	$link=mysqli_connect("localhost", "u1157881_st", "pasDatBas", "u1157881_bd");
-	$sqlSel="SELECT * FROM `st_user` WHERE log = '".$log."' AND pas = '".$pas."'";
-	$result = mysqli_query($link, $sqlSel);
-$res=mysqli_num_rows($result);
-if ($res>0){session_start(); $_SESSION['login']=$log; header('Location:https://schelcol-refork.ru/f.goryainov');
-setcookie("log", $log);}else{header('Location:https://schelcol-refork.ru/f.goryainov/log/auth.php');}
-?>
